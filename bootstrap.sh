78cf226935430061dc92237e4458dbcd7b132093
Andreas Maunz <andreas@maunz.de>
2014-11-18 10:08:44 +0100

    [master] added java and nodejs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
 apt-get update
-apt-get install -y tree flip vim git subversion curl wget
+apt-get install -y tree flip vim git subversion curl wget openjdk-7-jdk 
+curl -sL https://deb.nodesource.com/setup | bash -
+apt-get install -y nodejs
