3ba47f3dd30253bdc75402d6b1f4fc7b4114f134
Andreas Maunz <andreas@maunz.de>
2014-11-19 10:10:57 +0100

    [master] updated Vagrantfile and bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,25 @@
 apt-get update
-apt-get install -y tree flip vim git subversion
+
+# apt-file and update
+apt-get install -y apt-file
+apt-file update
+
+# standard tools
+apt-get install -y tree flip vim git subversion curl wget openjdk-7-jdk
+
+# nodejs
+curl -sL https://deb.nodesource.com/setup | bash -
+apt-get install -y nodejs
+
+# R
+apt-key adv --keyserver keys.gnupg.net --recv-key 381BA480
+echo "deb http://cran.rstudio.com/bin/linux/debian wheezy-cran3/" > /etc/apt/sources.list.d/cran.list
+apt-get update
+aptitude install -y r-base-core
+cd /tmp
+R --vanilla <<EOF
+  download.file("http://www.lepem.ufc.br/jaa/colorout_1.0-3.tar.gz", destfile = "colorout_1.0-3.tar.gz")
+  install.packages("colorout_1.0-3.tar.gz", type = "source", repos = NULL)
+EOF
+cd -
+
