bdb6214037fcdffb937332f41188b804a8754565
Andreas Maunz <andreas@maunz.de>
2015-01-13 10:33:51 +0100

    added apt-file update

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,16 +6,19 @@ apt-file update
 
 # standard tools
 apt-get install -y tree flip vim git subversion curl wget openjdk-7-jdk
+apt-get -y -f install
 
 # nodejs
 curl -sL https://deb.nodesource.com/setup | bash -
 apt-get install -y nodejs
+apt-get -y -f install
 
 # R
 apt-key adv --keyserver keys.gnupg.net --recv-key 381BA480
 echo "deb http://cran.rstudio.com/bin/linux/debian wheezy-cran3/" > /etc/apt/sources.list.d/cran.list
 apt-get update
 aptitude install -y r-base-core
+apt-get -y -f install
 cd /tmp
 R --vanilla <<EOF
   download.file("http://www.lepem.ufc.br/jaa/colorout_1.1-0.tar.gz", destfile = "colorout_1.1-0.tar.gz")
@@ -30,4 +33,3 @@ dpkg -i google-chrome-*.deb
 apt-get install -y -f
 cd -
 
-apt-get -y -f install
